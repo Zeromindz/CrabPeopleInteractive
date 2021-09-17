cec1e570e949298a3fd09f0e9f402ad502842b3c
DecJam <Declan@thedollerfamily.net>
2021-09-17 23:47:57 +1000

    Added Declan's Scene, Updated Sound, Menu managers

--- Assets/Scripts/Managers and controllers/MenuController.cs
+++ Assets/Scripts/Managers and controllers/MenuController.cs
@@ -6,13 +6,27 @@ using UnityEngine;
 //	Declan Doller
 //
 //
-	public enum MenuState
+
+public enum MenuState
+{
+	MAINMENU,
+	GAME,
+	GAMEPAUSED,
+	SETTINGS,
+	ENDSCREEN,
+}
+
+public struct MenuStackItem
+{
+	public GameObject UI;
+	public MenuState State;
+
+	public MenuStackItem(GameObject Canvas, MenuState state)
 	{
-		MAINMENU,
-		GAME,
-		GAMEPAUSED,
-		ENDSCREEN,
+		UI = Canvas;
+		State = state;
 	}
+}
 
 public class MenuController : MonoBehaviour
 {
@@ -26,6 +40,13 @@ public class MenuController : MonoBehaviour
 	{
 		get { return m_Instance; }
 	}
+	public GameObject m_MenuUI = null;
+	public GameObject m_GameUI = null;
+	public GameObject m_GamePausedUI = null;
+	public GameObject m_SettingsUI = null;
+	public GameObject m_EndScreenUI = null;
+	private GameObject m_CurrentUI = null;
+	Stack<MenuStackItem> m_UIStack = new Stack<MenuStackItem>();
 
 	public bool IsGamePaused
 	{
@@ -40,13 +61,147 @@ public class MenuController : MonoBehaviour
 			Destroy(this.gameObject);
 		else
 			m_Instance = this;
+
+		m_CurrentUI = m_MenuUI;
+		LoadMenu();
+		//m_State = MenuState.MAINMENU;
+	}
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Escape) && m_State == MenuState.GAME)
+		{
+			PauseGame();
+		}
+
+		else if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			ReturnToPreviousUI();
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+		
+		}
+	}
+
+	public void ReturnToPreviousUI()
+	{
+		if(m_UIStack.Count >= 2)
+		{
+			m_UIStack.Pop();
+			MenuStackItem menu = m_UIStack.Peek();
+			m_State = menu.State;
+			UpdateState();
+		}
+
+		else
+		{
+			MenuStackItem menu = m_UIStack.Peek();
+			m_State = menu.State;
+			UpdateState();
+		}
+	}
+
+
+	// Pauses the game
+	public void PauseGame()
+	{
+		Debug.Log("Pausing Game");
+		m_State = MenuState.GAMEPAUSED;
+		UpdateState();
+		m_UIStack.Push(new MenuStackItem(m_GamePausedUI, MenuState.GAMEPAUSED));
+	}
+
+	// Unpauses the game
+	public void UnPauseGame()
+	{
+		Debug.Log("Un-Pausing Game");
+		m_State = MenuState.GAME;
+		UpdateState();
+	}
+
+	// Loads the main menu
+	public void LoadMenu()
+	{
+		Debug.Log("Loading Menu");
+		m_State = MenuState.MAINMENU;
+		UpdateState();
+		m_UIStack.Clear();
+		m_UIStack.Push(new MenuStackItem(m_MenuUI, MenuState.MAINMENU));
+	}
+
+	// Starts the game
+	public void LoadGame()
+	{
+		Debug.Log("Loading: Game");
+		m_State = MenuState.GAME;
+		UpdateState();
+		m_UIStack.Clear();
+		m_UIStack.Push(new MenuStackItem(m_GameUI, MenuState.GAME));
+	}
+
+	// Loads the settings UI
+	public void LoadSettings()
+	{
+		Debug.Log("Loading: Settings");
+		m_State = MenuState.SETTINGS;
+		UpdateState();
+		m_UIStack.Push(new MenuStackItem(m_SettingsUI, MenuState.SETTINGS));
+	}
+
+	// Exits the application
+	public void ExitGame()
+	{
+		Debug.Log("Closing App");
+		Application.Quit();
 	}
 
 	// Sets the game state
 	public void SetState(MenuState state)
 	{
 		m_State = state;
+		UpdateState();
 	}
+
+	// Updates the states by activating the state
+	private void UpdateState()
+	{
+		m_CurrentUI.SetActive(false);
+
+		if(m_State == MenuState.MAINMENU)
+		{
+			m_MenuUI.SetActive(true);
+			m_CurrentUI = m_MenuUI;
+		}
+
+		if (m_State == MenuState.GAME)
+		{
+			m_GameUI.SetActive(true);
+			m_CurrentUI = m_GameUI;
+		}
+
+		if (m_State == MenuState.GAMEPAUSED)
+		{
+			m_GamePausedUI.SetActive(true);
+			m_CurrentUI = m_GamePausedUI;
+		}
+
+		if (m_State == MenuState.SETTINGS)
+		{
+			m_SettingsUI.SetActive(true);
+			m_CurrentUI = m_SettingsUI;
+		}
+
+		if (m_State == MenuState.ENDSCREEN)
+		{
+			m_EndScreenUI.SetActive(true);
+			m_CurrentUI = m_EndScreenUI;
+		}
+	}
+
+	// Cause Jayden wanted to be apart
+	public void JaydenWasHere()
+	{}
 
 	// Toggles pause
 	public void TogglePause()
--- Assets/Scripts/Managers and controllers/SoundManager.cs
+++ Assets/Scripts/Managers and controllers/SoundManager.cs
@@ -1,30 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 // Worked on by:
 //	Declan Doller
 //
 //
 
+public struct SliderInput
+{
+	public Slider Sliders;
+	public GameObject Input;
+	
+	public SliderInput(Slider slider, GameObject input)
+	{
+		Sliders = slider;
+		Input = input;
+	}
+}
+
 public class SoundManager : MonoBehaviour
 {
 	private static SoundManager m_Instance;
 	private float m_MainVolume = 1.0f;
 	private float m_MusicVolume = 1.0f;
-	private float m_OtherVolume = 1.0f;
+	private float m_SFXVolume = 1.0f;
 
 	public AudioSource m_GameMusic = null;
 	public AudioSource m_MenuMusic = null;
 	public AudioSource[] m_CollisionSounds = new AudioSource[10];
 	public AudioSource[] m_MenuSelectSounds = new AudioSource[10];
 
+	[Header ("Sliders")]
+	public Slider m_MainVolumeSlider = null;
+	public Slider m_MusicVolumeSlider = null;
+	public Slider m_SFXVolumeSlider = null;
+
+	[Header("InputFields")]
+	public GameObject m_MainVolumeInput = null;
+	public GameObject m_MusicVolumeInput = null;
+	public GameObject m_SFXVolumeInput = null;
+
+	private SliderInput m_Main;
+	private SliderInput m_Music;
+	private SliderInput m_SFX;
+
 	// Singleton instance
 	public static SoundManager Instance
 	{
 		get { return m_Instance; }
 	}
-
+	
 	// Called when script is being loaded
 	void Awake()
 	{
@@ -33,6 +61,14 @@ public class SoundManager : MonoBehaviour
 			Destroy(this.gameObject);
 		else
 			m_Instance = this;
+	}
+
+	// On start 
+	private void Start()
+	{
+		m_Main = new SliderInput(m_MainVolumeSlider, m_MainVolumeInput);
+		m_Music = new SliderInput(m_MusicVolumeSlider, m_MusicVolumeInput);
+		m_SFX = new SliderInput(m_SFXVolumeSlider, m_SFXVolumeInput);
 	}
 
 	// Sets the main volume
@@ -48,6 +84,26 @@ public class SoundManager : MonoBehaviour
 			volume = 0;
 		}
 		m_MainVolume = volume;
+		UpdateInputValue(m_Main, m_MainVolume);
+	}
+	public void SetMainVolume(string text)
+	{
+		int volume;
+		volume = int.Parse(text);
+
+		if(volume > 100)
+		{
+			volume = 100;
+		}
+
+		else if (volume < 0)
+		{
+			volume = 0;
+		}
+
+		m_MainVolume = (volume / 100);
+		UpdateSliderValue(m_Main, m_MainVolume);
+		
 	}
 
 	// Sets the music volume
@@ -64,10 +120,36 @@ public class SoundManager : MonoBehaviour
 		}
 
 		m_MusicVolume = volume;
+		UpdateInputValue(m_Music, m_MusicVolume);
+	}
+	public void SetMusicVolume(string text)
+	{
+		int volume;
+		bool success = int.TryParse(text, out volume);
+
+		if (success)
+		{
+			if (volume > 100)
+			{
+				volume = 100;
+			}
+
+			else if (volume < 0)
+			{
+				volume = 0;
+			}
+			m_MusicVolume = (volume / 100);
+			UpdateSliderValue(m_Music, m_MusicVolume);
+		}
+		else
+		{
+			Debug.Log("Failed to convert string to int: Music Volume");
+		}
+
 	}
 
-	// Sets the other volume
-	public void SetOtherVolume(float volume)
+	// Sets the SFX volume
+	public void SetSFXVolume(float volume)
 	{
 		if (volume > 1)
 		{
@@ -79,7 +161,32 @@ public class SoundManager : MonoBehaviour
 			volume = 0;
 		}
 
-		m_OtherVolume = volume;
+		m_SFXVolume = volume;
+		UpdateInputValue(m_SFX, m_SFXVolume);
+	}
+	public void SetSFXVolume(string text)
+	{
+		int volume;
+		bool success = int.TryParse(text, out volume);
+
+		if (success)
+		{
+			if (volume > 100)
+			{
+				volume = 100;
+			}
+
+			else if (volume < 0)
+			{
+				volume = 0;
+			}
+			m_SFXVolume = (volume / 100);
+			UpdateSliderValue(m_SFX, m_SFXVolume);
+		}
+		else
+		{
+			Debug.Log("Failed to convert string to int: SFX Volume");
+		}
 	}
 
 	// Plays the Game Music
@@ -104,6 +211,20 @@ public class SoundManager : MonoBehaviour
 	public void PlayMenuSelectSound(int index)
 	{
 		m_MenuSelectSounds[index].Play();
+	}
+
+	// Updates the slider value from Input fields
+	private void UpdateSliderValue(SliderInput sliderInput, float value)
+	{
+		sliderInput.Sliders.GetComponent<Scrollbar>().value = value/100;
+	}
+
+	// Updates the Input value based on the slider
+	private void UpdateInputValue(SliderInput sliderInput, float value)
+	{
+		int num = (int)(value * 100);
+		TextMeshProUGUI text = sliderInput.Input.GetComponent<TextMeshProUGUI>();
+		text.text = "" + num;
 	}
 }
 
