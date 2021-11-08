caf62ae3747b5b351301a6d91844692db4387a1f
DecJam <Declan@thedollerfamily.net>
2021-11-09 10:51:38 +1100

    Updated GameUI to represent information we want

--- Assets/Scripts/UI Scripts/GameUI.cs
+++ Assets/Scripts/UI Scripts/GameUI.cs
@@ -8,18 +8,12 @@ public class GameUI : MonoBehaviour
     private GameManager m_GameManager;
 
     [SerializeField] private TextMeshProUGUI m_TimerUI;
-    [SerializeField] private TextMeshProUGUI m_SpeedUI;
-    [SerializeField] private TextMeshProUGUI m_PassengerUI;
-    [SerializeField] private TextMeshProUGUI m_TrickUI;
-    [SerializeField] private TextMeshProUGUI m_CountDown;
-
-    private bool m_CountingDown = false;                            // IF the coundown is happening
-    private int m_CountDownTime = 4;                                // The time it takes to resume the game always set to the time + 1 so the player can see the full countdown
-    private float m_Timer = 0;										// The time that has elapsed since the countdown has started
+    [SerializeField] private TextMeshProUGUI m_CurrentSpeedText;
+    [SerializeField] private TextMeshProUGUI m_MaxSpeedUI;
 
     void Start()
     {
-        m_CountDown.SetText("");
+
         m_GameManager = FindObjectOfType<GameManager>();
     }
 
@@ -27,51 +21,21 @@ public class GameUI : MonoBehaviour
     {
         UpdateUI();
 
-        if (m_CountingDown)
-        {
-            CountDown();
-        }
     }
 
     // To be changed to the gamemanager singleton
     void UpdateUI()
     {
-        string timerText = "Time: " + (int)GameManager.Instance.GetCurrentTime();
-        m_TimerUI.text = timerText;
+        //string timerText = "Time: " + (int)GameManager.Instance.GetCurrentTime();
+        //m_TimerUI.text = timerText;
 
-        string speedText = "Speed: " + Mathf.Round(GameManager.Instance.m_BoatSpeed);
-        m_SpeedUI.text = speedText;
+        //string speedText = "Speed: " + Mathf.Round(Player);
+        //m_SpeedUI.text = speedText;
 
-        string passengerText = "Passengers: " + GameManager.Instance.m_Passengers;
-        m_PassengerUI.text = passengerText;
+        //string passengerText = "Passengers: " + GameManager.Instance.m_Passengers;
+        //m_PassengerUI.text = passengerText;
 
-        string trickText = "Trick: " + GameManager.Instance.m_CurrentTrickRotation;
-        m_TrickUI.text = trickText;
-    }
-
-    public void StartCountDown()
-	{
-        m_CountingDown = true;
-	}
-
-    /// <summary>
-	/// Called when starting or resuming the game
-	/// Counts down from 3
-	/// </summary>
-	private void CountDown()
-    {
-        m_Timer += Time.unscaledDeltaTime;
-        if (m_Timer >= m_CountDownTime)
-        {
-            m_CountingDown = false;
-            m_CountDown.SetText("");
-            Time.timeScale = 1;
-            m_Timer = 0;
-        }
-
-        else
-        {
-            m_CountDown.SetText("" + ((int)(m_CountDownTime - m_Timer)));
-        }
+        //string trickText = "Trick: " + GameManager.Instance.m_CurrentTrickRotation;
+        //m_TrickUI.text = trickText;
     }
 }
